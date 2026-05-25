51586cb679dcd4c321aeca6beea65f6d295c1fd8
Eduardito1818 <Eduardito1818@gmail.com>
2026-05-24 19:39:15 -0500

    restructuramos ell codigo de app.jsx y creamos un nuevo archivo para los paquetes y que esten separados para mayor rendimiento

---
+++ src/pages/PackageDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function PackageDetail() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center font-poppins">
+      <h1 className="text-3xl font-black uppercase tracking-widest">
+        Lienzo en Blanco: Detalles del Paquete
+      </h1>
+    </div>
+  );
+}
